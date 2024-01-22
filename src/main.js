b0e8f68fafcd2d2985cea06afdbb0ce5880efa2c
DenisBahuta <denisboguto@gmail.com>
2024-01-22 09:46:07 +0100

    Delete mistake in submit form

--- src/main.js
+++ src/main.js
@@ -1,91 +1,133 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
+import icon from '../src/img/octagon.svg';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-const form = document.querySelector('#search-form');
-const input = document.querySelector('.search-input');
-const gallery = document.querySelector('.gallery');
-const loader = document.querySelector('.loader');
+const formSearch = document.querySelector('.form');
+const imageList = document.querySelector('.gallery');
+const preload = document.querySelector('.preload');
 
-let page = 1;
-let searchQuery = '';
+const gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 
-const lightbox = new SimpleLightbox('.gallery a');
+formSearch.addEventListener('submit', handleSearch);
 
-function searchImages(query) {
-  const BASE_URL = 'https://pixabay.com/api';
-  const API_KEY = '41838546-9d950a50e841202e6c289d2dd';
+function handleSearch(event) {
+  event.preventDefault();
+  const searchQuery = event.currentTarget.elements.input.value;
 
-  const url = `${BASE_URL}?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=9`;
+  imageList.innerHTML = '';
 
-  loader.classList.add('show');
+  if (!searchQuery.trim()) {
+    iziToast.show({
+      title: '❕',
+      theme: 'light',
+      message: `Please, fill in the search field`,
+      messageSize: '20px',
+      messageColor: '#808080',
+      backgroundColor: '#e7fc44',
+      position: 'topLeft',
+      timeout: 3000,
+    });
+    return;
+  }
 
-  fetch(url)
-    .then(function (response) {
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      return response.json();
-    })
-    .then(function (data) {
-      const images = data.hits;
+  preload.classList.remove('is-hidden');
 
-      if (images.length === 0) {
+  fetchImages(searchQuery)
+    .then(data => {
+      if (data.hits.length === 0) {
         iziToast.show({
-          title: 'Error',
+          iconUrl: icon,
+          theme: 'dark',
           message:
             'Sorry, there are no images matching your search query. Please try again!',
-          theme: 'dark',
-          position: 'topCenter',
-          timeout: 3000,
+          messageSize: '16px',
+          messageColor: 'white',
+          backgroundColor: '#EF4040',
+          position: 'topRight',
+          timeout: 5000,
         });
-      } else {
-        if (page === 1) {
-          gallery.innerHTML = '';
-        }
-
-        images.forEach(function (image) {
-          const card = `<div class="card">
-              <a href="${image.largeImageURL}">
-                <img src="${image.webformatURL}" alt="${image.tags}">
-              </a>
-              <div class="card-info">
-                <p>Likes: ${image.likes}</p>
-                <p>Views: ${image.views}</p>
-                <p>Comments: ${image.comments}</p>
-                <p>Downloads: ${image.downloads}</p>
-              </div>
-            </div>`;
-          gallery.insertAdjacentHTML('beforeend', card);
-        });
-
-        lightbox.refresh();
-
-        page++;
       }
+      imageList.innerHTML = createMarkup(data.hits);
+      gallery.refresh();
     })
-    .catch(function (error) {
-      iziToast.show({
-        title: 'Error',
-        message:
-          'Sorry, there was an error processing your request. Please try again later!',
-        theme: 'dark',
-        position: 'topCenter',
-        timeout: 3000,
-      });
-      console.log(error);
-    })
-    .finally(function () {
-      loader.classList.remove('show');
-    });
+    .catch(handleError)
+    .finally(() => preload.classList.add('is-hidden'));
+
+  event.currentTarget.reset();
 }
 
-form.addEventListener('submit', function (event) {
-  event.preventDefault();
-  searchQuery = input.value.trim();
-  if (searchQuery !== '') {
-    searchImages(searchQuery);
-    form.reset();
-  }
-});
+function fetchImages(value) {
+  const BASE_URL = 'https://pixabay.com/api';
+
+  const searchParams = new URLSearchParams({
+    key: '41838546-9d950a50e841202e6c289d2dd',
+    q: value,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: 'true',
+  });
+
+  return fetch(`${BASE_URL}/?${searchParams}`).then(res => {
+    if (!res.ok) {
+      throw new Error(res.status);
+    }
+    return res.json();
+  });
+}
+
+function createMarkup(arr) {
+  return arr
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) =>
+        `<li class="gallery-item">
+        <a class="gallery-link" href="${largeImageURL}">
+           <img
+            class="gallery-image"
+            src="${webformatURL}"
+            alt="${tags}"
+          />
+        </a>
+        <div class="container-additional-info">
+        <div class="container-descr-inner"><p class="description">Likes</p><span class="description-value">${likes}</span></div>
+        
+        <div class="container-descr-inner"><p class="description">Views</p><span class="description-value">${views}</span></div>
+        
+
+        <div class="container-descr-inner"><p class="description">Comments</p><span class="description-value">${comments}</span></div>
+        
+
+        <div class="container-descr-inner"><p class="description">Downloads</p><span class="description-value">${downloads}</span></div>
+        
+        </div>
+      </li>`
+    )
+    .join('');
+}
+
+function handleError(err) {
+  console.error(err);
+  imageList.innerHTML = '';
+  iziToast.show({
+    iconUrl: icon,
+    theme: 'dark',
+    message: 'Sorry, there is a problem with connection with the server.',
+    messageSize: '16px',
+    messageColor: 'white',
+    backgroundColor: '#EF4040',
+    position: 'center',
+    timeout: 5000,
+  });
+}
